e526e0fce3fefd23ef6e734e3911e5259aed8c6f
Amin Bashi <amin@leafpoints.com>
2016-10-06 10:28:04 -0400

    Leafpoints App
    LP was a loyalty program that incentivized users to purchase
    eco-friendly products (online and offline).

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!/System/Library/Frameworks/Python.framework/Versions/2.6/bin/python2.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
